2d7758b7c7798f98dbdc89ccfd99b80b6a16d23c
Umut Can Karçe <umutcankarce1@gmail.com>
2024-12-10 02:05:47 +0300

    rightbar search input design :fire:

--- src/layouts/main/rightbar/index.jsx
+++ src/layouts/main/rightbar/index.jsx
@@ -1,7 +1,9 @@
+import Search from "~/layouts/main/rightbar/search";
+
 export default function RightBar (){ 
     return (
         <aside className="w-[350px] mr-2.5">
-            Right sidebar
+            <Search/>
         </aside>
     )
 }
--- src/layouts/main/sidebar/account/index.jsx
+++ src/layouts/main/sidebar/account/index.jsx
@@ -8,7 +8,7 @@ export default function Account(){
         <div className="mt-auto">
             <Popover className="relative">
                 <PopoverButton 
-                className="my-3 p-3 rounded-full hover:bg-[#eff3f41a] w-full flex text-left items-center outline-none">
+                className="my-3 p-3 rounded-full hover:bg-[#eff3f41a] w-full flex transition-colors text-left items-center outline-none">
                 <img src={account.avatar} alt="" className="w-10 h-10 rounded-full" />
                 <div className="mx-3 text-[15px]">
                     <h6 className="font-bold leading-[20px]">{account.fullName}</h6>
